0da5c0681b4542bd7a7bc32e8885356faaefd790
TheIncredibleVee <cehvinaysachdeva@gmail.com>
2021-07-15 16:40:40 +0530

    Tweaks

--- src/portfolio.js
+++ src/portfolio.js
@@ -30,8 +30,8 @@ const socialMediaLinks = {
   gitlab: "https://gitlab.com/saadpasta",
   facebook: "https://www.facebook.com/TheIncredibleVee/",
   instagram: "https://www.instagram.com/theincrediblevee/",
-  //medium: "https://medium.com/@saadpasta",
-  //stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  medium: "https://medium.com/@saadpasta",
+  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram and Twitter are also supported in the links!
   display: true // Set true to display this section, defaults to false
 };
