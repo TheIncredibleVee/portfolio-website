d16f487290b4f2f9dd22cb08bccc1a569131393c
Vinay <Vinay>
2022-01-09 07:53:56 +0530

    Git token fix

---
+++ build/precache-manifest.f65025a825d2387a29d0094f4048576f.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ce800d9ff83e6790440e4f8fb82811",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "61458009b03ed7643c31",
+    "url": "/portfolio-website/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "776c3537466a1d6248c8",
+    "url": "/portfolio-website/static/css/4.5c57288c.chunk.css"
+  },
+  {
+    "revision": "7b4911b1b941f4366bc1",
+    "url": "/portfolio-website/static/css/main.e38af0f8.chunk.css"
+  },
+  {
+    "revision": "ed17a22f7b3ab45ee228",
+    "url": "/portfolio-website/static/js/2.db8dad7c.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/portfolio-website/static/js/2.db8dad7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61458009b03ed7643c31",
+    "url": "/portfolio-website/static/js/3.a10d5e1a.chunk.js"
+  },
+  {
+    "revision": "776c3537466a1d6248c8",
+    "url": "/portfolio-website/static/js/4.bb1c3655.chunk.js"
+  },
+  {
+    "revision": "7b4911b1b941f4366bc1",
+    "url": "/portfolio-website/static/js/main.e7f9ece5.chunk.js"
+  },
+  {
+    "revision": "f9273e3e435b00fc9a27",
+    "url": "/portfolio-website/static/js/runtime-main.31889e65.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/portfolio-website/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "411f1ccbe6a02ef58c20ac6e1f45a9ef",
+    "url": "/portfolio-website/static/media/DTU.411f1ccb.png"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/portfolio-website/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "f4a4dd6ccdf9b17a495a88d111b1a066",
+    "url": "/portfolio-website/static/media/alpha.f4a4dd6c.png"
+  },
+  {
+    "revision": "57354593287271f95a77bfcc552bef6f",
+    "url": "/portfolio-website/static/media/bulleton.57354593.png"
+  },
+  {
+    "revision": "86b18d4d869db5cb47c0be4cc083440b",
+    "url": "/portfolio-website/static/media/ceh.86b18d4d.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio-website/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio-website/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "6eaa932c32048fe48eb78401c852430d",
+    "url": "/portfolio-website/static/media/expendo.6eaa932c.png"
+  },
+  {
+    "revision": "55d3560e93ba6e8e604f43b0a089c3c3",
+    "url": "/portfolio-website/static/media/google.55d3560e.png"
+  },
+  {
+    "revision": "58168a99da744f60ff68ae4ffe20085d",
+    "url": "/portfolio-website/static/media/isb.58168a99.png"
+  },
+  {
+    "revision": "5f09a522da8d342149c256579c029291",
+    "url": "/portfolio-website/static/media/lancer.5f09a522.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio-website/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "9e0c31bfb89f8cf1883744fc241d715b",
+    "url": "/portfolio-website/static/media/mic.9e0c31bf.jfif"
+  },
+  {
+    "revision": "3f90df6a5620a66b42fc488898872e2c",
+    "url": "/portfolio-website/static/media/pen.3f90df6a.jpg"
+  },
+  {
+    "revision": "22dfef85c0e10ebc494219294002f116",
+    "url": "/portfolio-website/static/media/sas.22dfef85.webp"
+  },
+  {
+    "revision": "39898b82738cde0b6ed5aad7c276dac7",
+    "url": "/portfolio-website/static/media/search.39898b82.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio-website/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "aa165d1e67c7e3776ddd960dff7ce735",
+    "url": "/portfolio-website/static/media/stock.aa165d1e.webp"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio-website/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "e85de4e3cc695f14c95162c87ecd9bc7",
+    "url": "/portfolio-website/static/media/ub.e85de4e3.png"
+  },
+  {
+    "revision": "392a4d0f414c9210676587e533a0fe7a",
+    "url": "/portfolio-website/static/media/ucd.392a4d0f.jpg"
+  }
+]);
